3665ec02f58e2e9c2359268004642fb45b1cf5d0
Ajitem Sahasrabuddhe <ajitem.s@outlook.com>
2018-08-12 14:30:44 +0530

    Fixed Bug: Return on invalid condition

--- server/middleware/filter-incoming-requests.go
+++ server/middleware/filter-incoming-requests.go
@@ -7,9 +7,10 @@ import (
 
 func FilterIncomingRequestsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.Header.Get("Content-Type") == "application/json" {
+		if r.Header.Get("Content-Type") != "application/json" {
 			w.WriteHeader(http.StatusBadRequest)
 			fmt.Fprintln(w, "{'status': 'failure', 'error': 'API only accepts JSON input. Please format the input properly or ensure that correct headers are set in the request header.'}")
+			return
 		}
 		next.ServeHTTP(w, r)
 	})
