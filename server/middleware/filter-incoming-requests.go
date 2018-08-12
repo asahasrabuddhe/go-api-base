aa59b8799fd39c2bace256618725dee819fe1aeb
Ajitem Sahasrabuddhe <ajitem.s@outlook.com>
2018-08-12 18:55:28 +0530

    Updated JSON Middleware to use Response Type

--- server/middleware/filter-incoming-requests.go
+++ server/middleware/filter-incoming-requests.go
@@ -1,7 +1,8 @@
 package middleware
 
 import (
-	"fmt"
+	"encoding/json"
+	"gitlab.com/asahasrabuddhe/go-api-base/response"
 	"net/http"
 )
 
@@ -9,9 +10,12 @@ func FilterIncomingRequestsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if r.Method == "POST" && r.Header.Get("Content-Type") != "application/json" {
 			w.WriteHeader(http.StatusBadRequest)
-			fmt.Fprintln(w, "{'status': 'failure', "+
-				"'error': 'API only accepts JSON input. Please format the input properly or ensure that correct headers are set in the request header.'}")
-			return
+			res := response.Response{
+				Success: false,
+				Message: "API only accepts JSON input. Please format the input properly or ensure that correct headers are set in the request header.",
+			}
+			encoder := json.NewEncoder(w)
+			encoder.Encode(&res)
 		}
 		next.ServeHTTP(w, r)
 	})
