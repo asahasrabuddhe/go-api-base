9a8bb304ff21a8f0ef4ac0694d1faaf842897817
Ajitem Sahasrabuddhe <ajitem.s@outlook.com>
2018-08-12 18:22:26 +0530

    Code Formatting

--- server/middleware/filter-incoming-requests.go
+++ server/middleware/filter-incoming-requests.go
@@ -9,7 +9,8 @@ func FilterIncomingRequestsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if r.Header.Get("Content-Type") != "application/json" {
 			w.WriteHeader(http.StatusBadRequest)
-			fmt.Fprintln(w, "{'status': 'failure', 'error': 'API only accepts JSON input. Please format the input properly or ensure that correct headers are set in the request header.'}")
+			fmt.Fprintln(w, "{'status': 'failure', "+
+				"'error': 'API only accepts JSON input. Please format the input properly or ensure that correct headers are set in the request header.'}")
 			return
 		}
 		next.ServeHTTP(w, r)
