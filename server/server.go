e2ee487cf397f36f37184df71972a369be0cab58
Ajitem Sahasrabuddhe <ajitem.s@outlook.com>
2018-08-12 10:53:26 +0530

    Added Json Middleware

--- server/server.go
+++ server/server.go
@@ -1,15 +1,19 @@
 package server
 
 import (
-	"net/http"
 	"fmt"
-	"gitlab.com/asahasrabuddhe/go-api-base/router"
 	"github.com/gorilla/mux"
 	db "gitlab.com/asahasrabuddhe/go-api-base/database"
+	"gitlab.com/asahasrabuddhe/go-api-base/router"
+	"gitlab.com/asahasrabuddhe/go-api-base/server/middleware"
+	"net/http"
 )
 
 func Init(username, password, database, host, port string) {
 	router.Router = mux.NewRouter()
+
+	router.Router.Use(middleware.FilterIncomingRequestsMiddleware)
+
 	db.Open(username, password, database, host, port)
 }
 
