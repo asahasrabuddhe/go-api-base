6c1cbe1ffea3c708f87ca87d75da5afabc8d8117
Ajitem Sahasrabuddhe <ajitem.s@outlook.com>
2018-09-02 21:31:13 +0530

    Bug Fix: Update incorrect path for image writer

--- request/image-validation-request.go
+++ request/image-validation-request.go
@@ -29,7 +29,7 @@ func (iv *ImageValidator) Validate() error {
 			return errors.New("Invalid image data. Please try again.")
 		} else {
 			iv.ImageData = strconv.Itoa(int(time.Now().Unix())) + "_" + iv.Name + ".png"
-			if file, err := os.OpenFile(viper.GetString("app.public_path")+"/images/"+iv.Name, os.O_CREATE|os.O_WRONLY, 0644); err != nil {
+			if file, err := os.OpenFile(viper.GetString("public_path")+"/images/"+iv.ImageData, os.O_CREATE|os.O_WRONLY, 0644); err != nil {
 				return err
 			} else {
 				png.Encode(file, pngImage)
@@ -41,7 +41,7 @@ func (iv *ImageValidator) Validate() error {
 		} else {
 			strconv.Itoa(int(time.Now().Unix()))
 			iv.ImageData = strconv.Itoa(int(time.Now().Unix())) + "_" + iv.Name + ".jpg"
-			if file, err := os.OpenFile(viper.GetString("app.public_path")+"/images/"+iv.ImageData, os.O_CREATE|os.O_WRONLY, 0644); err != nil {
+			if file, err := os.OpenFile(viper.GetString("public_path")+"/images/"+iv.ImageData, os.O_CREATE|os.O_WRONLY, 0644); err != nil {
 				return err
 			} else {
 				jpeg.Encode(file, jpegImage, &jpeg.Options{Quality: 75})
@@ -52,7 +52,7 @@ func (iv *ImageValidator) Validate() error {
 			return errors.New("Invalid image data. Please try again.")
 		} else {
 			iv.ImageData = strconv.Itoa(int(time.Now().Unix())) + "_" + iv.Name + ".gif"
-			if file, err := os.OpenFile(viper.GetString("app.public_path")+"/images/"+iv.ImageData, os.O_CREATE|os.O_WRONLY, 0644); err != nil {
+			if file, err := os.OpenFile(viper.GetString("public_path")+"/images/"+iv.ImageData, os.O_CREATE|os.O_WRONLY, 0644); err != nil {
 				return err
 			} else {
 				gif.Encode(file, gifImage, &gif.Options{})
