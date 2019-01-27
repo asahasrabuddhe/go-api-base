65b7eadd2261bef4b85bb8f65c3bb948ef5dfb29
Ajitem Sahasrabuddhe <ajitem.sahasrabuddhe@perennialsys.com>
2019-01-27 14:44:54 +0530

    send token separately for login

--- response/response.go
+++ response/response.go
@@ -4,5 +4,6 @@ type Response struct {
 	Success bool        `json:"success"`
 	Message string      `json:"message,omitempty"`
 	Data    interface{} `json:"data,omitempty"`
+	Token   string      `json:"token,omitempty"`
 	Total   int         `json:"total,omitempty"`
 }
